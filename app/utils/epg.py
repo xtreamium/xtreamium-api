d7500fa712e3f4559ae42ffc1653218517f68b06
Fergal Moran <fergal.moran@gmail.com>
2024-05-20 22:41:04 +0100

    Rejigged the stuff

---
+++ app/utils/epg.py
@@ -0,0 +1,40 @@
+import os
+import tempfile
+import time
+
+import requests
+from xdg_base_dirs import xdg_config_dirs, xdg_config_home, xdg_cache_home
+
+from app.services.logger import logger
+from app.utils import xmltv
+from app.utils.time_utils import is_file_older_cache_time
+
+
+class EPGParser:
+  def __init__(self, url):
+    self._epg_url = url
+    self._programs = {}
+    cache_dir = os.getenv("CACHE_PATH") or os.path.join(xdg_cache_home(), "xtreamium")
+    if not os.path.exists(cache_dir):
+      os.makedirs(cache_dir)
+
+    self._cache_file = os.path.join(cache_dir, "epg.xml")
+
+  def cache_epg(self):
+    if not is_file_older_cache_time(self._cache_file):
+      return
+
+    logger.debug(f"Downloading EPG to {self._cache_file}")
+    data = requests.get(self._epg_url)
+    with open(self._cache_file, 'wb') as file:
+      file.write(data.content)
+
+    logger.debug("Parsing EPG")
+
+    self._programs = xmltv.read_programmes(open(self._cache_file, 'r'))
+    logger.debug("EPG parsed")
+
+  def get_listings(self, channel_id):
+    listings = [d for d in self._programs if d['channel'] ==
+                channel_id and d['stop'] > int(time.time())]
+    return listings
