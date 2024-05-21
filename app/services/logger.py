415ef5e12d0e046017548462041de6769e805afc
Fergal Moran <fergal.moran@gmail.com>
2024-05-21 17:12:04 +0100

    Weird late night merge?

--- app/services/logger.py
+++ app/services/logger.py
@@ -1,4 +1,15 @@
 import logging
+import sys
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
+formatter = logging.Formatter(
+  fmt='%(levelname)s %(asctime)s %(message)s'
+)
+stream_handler = logging.StreamHandler(sys.stdout)
+file_handler = logging.FileHandler("/tmp/xtreamium.log")
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.handlers = [stream_handler, file_handler]
--- server/utils/cache.py
+++
@@ -1,2 +0,0 @@
-class Cache:
-    authData = {}
