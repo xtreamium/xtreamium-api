f5416957c095d9517dfb454f78bfc376f9e4f2f4
Fergal Moran <fergal.moran@gmail.com>
2025-09-11 23:34:46 +0100

    Reformats

--- app/services/config.py
+++ app/services/config.py
@@ -5,21 +5,17 @@ from pydantic.v1 import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Xtreamium Backend"
-    API_PATH: str = "/api/v1"
-    JWT_SECRET: str = "446974df0ad13e965b7b3a169aa82f08d07bf12a9feb3696e25844c0c677720e"
+  PROJECT_NAME: str = "Xtreamium Backend"
+  API_PATH: str = "/api/v1"
+  JWT_SECRET: str = "446974df0ad13e965b7b3a169aa82f08d07bf12a9feb3696e25844c0c677720e"
 
-    # TODO: need to make this a per-user cache
-    EPG_URL = AnyHttpUrl(
-        "http://sr71.biz/xmltv.php?username=Juicy5Bus&password=zYhuTE4qte"
-    )
-    BACKEND_CORS_ORIGINS: List = [
-        "https://xtreamium.dev.fergl.ie:3000",
-        "https://streams.dev.fergl.ie:3000",
-        "https://streams.ferg.al",
-        "http://127.0.0.1:35729",
-        "http://localhost:35729",
-    ]
+  BACKEND_CORS_ORIGINS: List = [
+    "https://xtreamium.dev.fergl.ie:3000",
+    "https://streams.dev.fergl.ie:3000",
+    "https://streams.ferg.al",
+    "http://127.0.0.1:35729",
+    "http://localhost:35729",
+  ]
 
 
 settings = Settings()
--- app/utils/epg_parser.py
+++ app/utils/epg_parser.py
@@ -13,33 +13,31 @@ logger = get_logger(__name__)
 
 
 class EPGParser:
-    def __init__(self, url):
-        self._epg_url = url
-        self._programs = {}
-        cache_dir = (os.getenv("CACHE_PATH") or
-                     os.path.join(xdg_cache_home(), "xtreamium"))
-        if not os.path.exists(cache_dir):
-            os.makedirs(cache_dir)
+  def __init__(self, url):
+    self._epg_url = url
+    self._programs = {}
+    cache_dir = (os.getenv("CACHE_PATH") or
+                 os.path.join(xdg_cache_home(), "xtreamium"))
+    if not os.path.exists(cache_dir):
+      os.makedirs(cache_dir)
 
-        self._cache_file = os.path.join(cache_dir, "epg.xml")
+    self._cache_file = os.path.join(cache_dir, "epg.xml")
 
-    def cache_epg(self):
-        if (not os.path.isfile(self._cache_file) or
-            is_file_older_cache_time(
-                self._cache_file)):
-            logger.debug(f"Downloading EPG to {self._cache_file}")
-            data = requests.get(self._epg_url)
-            with open(self._cache_file, 'wb') as file:
-                file.write(data.content)
+  def cache_epg(self):
+    if not os.path.isfile(self._cache_file) or is_file_older_cache_time(self._cache_file):
+      logger.debug(f"Downloading EPG to {self._cache_file}")
+      data = requests.get(self._epg_url)
+      with open(self._cache_file, 'wb') as file:
+        file.write(data.content)
 
-        logger.debug("Parsing EPG")
-        self._programs = xmltv.read_programmes(open(self._cache_file, 'r'))
-        logger.debug("EPG parsed")
+    logger.debug("Parsing EPG")
+    self._programs = xmltv.read_programmes(open(self._cache_file, 'r'))
+    logger.debug("EPG parsed")
 
-    def get_listings(self, channel_id):
-        listings = [d for d in self._programs if d['channel'] ==
-                    channel_id and d['stop'] > int(time.time())]
-        return listings
+  def get_listings(self, channel_id):
+    listings = [d for d in self._programs if d['channel'] ==
+                channel_id and d['stop'] > int(time.time())]
+    return listings
 
 
 epg_parser = EPGParser(settings.EPG_URL)
