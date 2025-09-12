678c2f37651a420f74f5ebfe61d879633009a15f
Fergal Moran <fergal.moran@gmail.com>
2025-09-12 16:17:40 +0100

    Add channel schemas

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .server import Server
+from .epg import EPG
+from .channel import Channel
+from .programme import Programme
+
+__all__ = ["User", "Server", "EPG", "Channel", "Programme"]
---
+++ app/models/program.py
@@ -0,0 +1,141 @@
+import datetime as dt
+import json
+
+import sqlalchemy as sa
+from sqlalchemy import orm as orm
+
+from app import database
+
+
+class Programme(database.Base):
+    class Config:
+        from_attributes = True
+
+    __tablename__ = "programmes"
+
+    # Primary key
+    id = sa.Column(sa.Integer, primary_key=True, index=True)
+
+    # Foreign key to channel
+    channel_id = sa.Column(sa.Integer, sa.ForeignKey(
+        "channels.id"), nullable=False)
+
+    # Core programme data
+    # XMLTV format: "20240901120000 +0000"
+    start_time = sa.Column(sa.String, nullable=False, index=True)
+    # XMLTV format: "20240901130000 +0000"
+    stop_time = sa.Column(sa.String, nullable=True, index=True)
+
+    # Optional programme attributes
+    pdc_start = sa.Column(sa.String, nullable=True)
+    vps_start = sa.Column(sa.String, nullable=True)
+    showview = sa.Column(sa.String, nullable=True)
+    videoplus = sa.Column(sa.String, nullable=True)
+    clumpidx = sa.Column(sa.String, default="0/1")
+
+    # Programme content - stored as JSON for flexibility
+    titles = sa.Column(sa.Text, nullable=True)  # JSON array of title data
+    # JSON array of subtitle data
+    sub_titles = sa.Column(sa.Text, nullable=True)
+    # JSON array of description data
+    descriptions = sa.Column(sa.Text, nullable=True)
+    credits = sa.Column(sa.Text, nullable=True)  # JSON object of credits data
+    date = sa.Column(sa.String, nullable=True)  # Programme date
+    # JSON array of category data
+    categories = sa.Column(sa.Text, nullable=True)
+    keywords = sa.Column(sa.Text, nullable=True)  # JSON array of keyword data
+    # JSON object of language data
+    language = sa.Column(sa.Text, nullable=True)
+    # JSON object of original language data
+    orig_language = sa.Column(sa.Text, nullable=True)
+    length = sa.Column(sa.Text, nullable=True)  # JSON object of length data
+    icons = sa.Column(sa.Text, nullable=True)  # JSON array of icon data
+    urls = sa.Column(sa.Text, nullable=True)  # JSON array of URL data
+    countries = sa.Column(sa.Text, nullable=True)  # JSON array of country data
+    # JSON array of episode number data
+    episode_nums = sa.Column(sa.Text, nullable=True)
+    video = sa.Column(sa.Text, nullable=True)  # JSON object of video data
+    audio = sa.Column(sa.Text, nullable=True)  # JSON object of audio data
+    # JSON object of previously shown data
+    previously_shown = sa.Column(sa.Text, nullable=True)
+    # JSON object of premiere data
+    premiere = sa.Column(sa.Text, nullable=True)
+    # JSON object of last chance data
+    last_chance = sa.Column(sa.Text, nullable=True)
+    new = sa.Column(sa.Boolean, default=False)  # New programme flag
+    # JSON array of subtitle data
+    subtitles = sa.Column(sa.Text, nullable=True)
+    ratings = sa.Column(sa.Text, nullable=True)  # JSON array of rating data
+    # JSON array of star rating data
+    star_ratings = sa.Column(sa.Text, nullable=True)
+    reviews = sa.Column(sa.Text, nullable=True)  # JSON array of review data
+    images = sa.Column(sa.Text, nullable=True)  # JSON array of image data
+
+    # Metadata
+    date_created = sa.Column(
+        sa.DateTime, default=dt.datetime.now(dt.timezone.utc))
+    date_last_updated = sa.Column(
+        sa.DateTime, default=dt.datetime.now(dt.timezone.utc))
+
+    # Relationships
+    channel = orm.relationship("Channel", back_populates="programmes")
+
+    # Index for performance - commonly queried together
+    __table_args__ = (
+        sa.Index('idx_programme_channel_start', 'channel_id', 'start_time'),
+        sa.Index('idx_programme_start_stop', 'start_time', 'stop_time'),
+    )
+
+    def get_titles(self):
+        """Return parsed titles as Python objects"""
+        if self.titles:
+            return json.loads(self.titles)
+        return []
+
+    def set_titles(self, titles):
+        """Store titles as JSON"""
+        self.titles = json.dumps(titles)
+
+    def get_descriptions(self):
+        """Return parsed descriptions as Python objects"""
+        if self.descriptions:
+            return json.loads(self.descriptions)
+        return []
+
+    def set_descriptions(self, descriptions):
+        """Store descriptions as JSON"""
+        self.descriptions = json.dumps(descriptions)
+
+    def get_categories(self):
+        """Return parsed categories as Python objects"""
+        if self.categories:
+            return json.loads(self.categories)
+        return []
+
+    def set_categories(self, categories):
+        """Store categories as JSON"""
+        self.categories = json.dumps(categories)
+
+    def get_credits(self):
+        """Return parsed credits as Python objects"""
+        if self.credits:
+            return json.loads(self.credits)
+        return {}
+
+    def set_credits(self, credits):
+        """Store credits as JSON"""
+        self.credits = json.dumps(credits)
+
+    # Helper methods for common JSON fields
+    def get_json_field(self, field_name):
+        """Generic method to get JSON field data"""
+        field_value = getattr(self, field_name, None)
+        if field_value:
+            return json.loads(field_value)
+        return [] if field_name in ['titles', 'sub_titles', 'descriptions', 'categories',
+                                    'keywords', 'icons', 'urls', 'countries', 'episode_nums',
+                                    'subtitles', 'ratings', 'star_ratings', 'reviews', 'images'] else {}
+
+    def set_json_field(self, field_name, value):
+        """Generic method to set JSON field data"""
+        setattr(self, field_name, json.dumps(value) if value else None)
